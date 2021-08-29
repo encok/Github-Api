06d6a6205b8254ee4008470d0904605bf4dcd576
enock <enckkipronoh@gmail.com>
2021-08-30 00:13:35 +0300

    adding the repos display section

--- githubApi/src/environments/dist/environment.js
+++ githubApi/src/environments/dist/environment.js
@@ -1,12 +1,13 @@
+"use strict";
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  Token:'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'
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: false,
+    Token: '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'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
